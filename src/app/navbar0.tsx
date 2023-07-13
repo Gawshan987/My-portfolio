00b55b3fbaa19044ade929d9f0075fac5ac1d1bb
Gawshan987 <gawshan.sri@gmail.com>
2023-07-13 16:23:22 -0400

    fix background

--- src/app/navbar0.tsx
+++
@@ -1,9 +0,0 @@
-import NavbarItem from "./NavbarItem";
-
-export default function Navbar() {
-    return <div className="flex flex-col-reverse">
-      
-        <NavbarItem title={"Gawshan"} />
- 
-    </div>
-}
